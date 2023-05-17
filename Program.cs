50c9064aa3db88cc4345471c797c822c7ba8a5ae
HP <HP@DESKTOP-4Q8V1JF>
2023-05-17 12:44:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace calculate_the_vowels_from_the_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter any string");
+            var str = Console.ReadLine();
+
+            int VowelsCounter = 0;
+            foreach (var item in str)
+            {
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
+                    item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+                {
+                    VowelsCounter++;
+                }
+
+            }
+            Console.WriteLine("Number of Vowels = " + VowelsCounter);
+        }
+    }
+}
